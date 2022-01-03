d25e99ffc619585d65166777bafe182fff5cbf12
Raymond Klauwer <klauwerraymond@gmail.com>
2022-01-03 08:38:09 -0600

    update homepage, navbar, responsive

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "/restaurante", to: "pages#restaurante"
   get "/habitacions", to: "pages#habitacions"
+  get "/menu-pdf", to: "pages#menu-pdf"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
